bb5c73e32d919dd3bb65697a4355fcb1d0ef348f
Neeru1610 <dummy@gmail.com>
2026-01-18 18:26:19 +0000

    message day 3

--- server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
@@ -1,40 +1,70 @@
 package com.edutech.progressive.service.impl;
-
+ 
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
-
+ 
 import com.edutech.progressive.dao.CricketerDAO;
 import com.edutech.progressive.entity.Cricketer;
 import com.edutech.progressive.service.CricketerService;
-
-public class CricketerServiceImplJdbc  implements CricketerService{
-    private final CricketerDAO cricketerDAO;
+ 
+public class CricketerServiceImplJdbc implements CricketerService {
+    private CricketerDAO cricketerDAO;
+ 
     public CricketerServiceImplJdbc(CricketerDAO cricketerDAO) {
         this.cricketerDAO = cricketerDAO;
     }
-
+ 
     @Override
-    public List<Cricketer> getAllCricketers() {
-        return null;
+    public List<Cricketer> getAllCricketers() throws SQLException {
+        try {
+            return cricketerDAO.getAllCricketers();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-
+ 
     @Override
-    public Integer addCricketer(Cricketer cricketer) {
-        return -1;
+    public Integer addCricketer(Cricketer cricketer) throws SQLException {
+        try {
+            return cricketerDAO.addCricketer(cricketer);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-
+ 
     @Override
-    public List<Cricketer> getAllCricketersSortedByExperience() {
-        return null;
+    public List<Cricketer> getAllCricketersSortedByExperience() throws SQLException {
+        try {
+            List<Cricketer> sortCricketers = cricketerDAO.getAllCricketers();
+            Collections.sort(sortCricketers);
+            return sortCricketers;
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-    public void updateCricketer(Cricketer cricketer){
-
+ 
+    @Override
+    public void updateCricketer(Cricketer cricketer) throws SQLException {
+        try {
+            cricketerDAO.updateCricketer(cricketer);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-    public void deleteCricketer(int cricketerId){
-
+ 
+    @Override
+    public void deleteCricketer(int cricketerId) throws SQLException {
+        cricketerDAO.deleteCricketer(cricketerId);
     }
-    public Cricketer getCricketerById(int cricketerId){
-        return null;
+ 
+    @Override
+    public Cricketer getCricketerById(int cricketerId) throws SQLException {
+        try {
+            return cricketerDAO.getCricketerById(cricketerId);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-
-
+ 
 }
--- server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJdbc.java
@@ -1,42 +1,59 @@
 package com.edutech.progressive.service.impl;
-
+ 
+import java.sql.SQLException;
 import java.util.List;
-
+ 
 import com.edutech.progressive.dao.MatchDAO;
 import com.edutech.progressive.entity.Match;
 import com.edutech.progressive.service.MatchService;
-
+ 
 public class MatchServiceImplJdbc implements MatchService {
-    private final MatchDAO matchDAO;
+    private MatchDAO matchDAO;
+ 
     public MatchServiceImplJdbc(MatchDAO matchDAO) {
         this.matchDAO = matchDAO;
     }
-
+ 
     @Override
-    public List<Match> getAllMatches() {
-        return null;
+    public List<Match> getAllMatches() throws SQLException {
+        try {
+            return matchDAO.getAllMatches();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+        }
+ 
     }
-
+ 
     @Override
-    public Match getMatchById(int matchId) {
-        return null;
+    public Match getMatchById(int matchId) throws SQLException {
+        try {
+            return matchDAO.getMatchById(matchId);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-
+ 
     @Override
-    public Integer addMatch(Match match) {
-        return -1;
+    public Integer addMatch(Match match) throws SQLException {
+        try {
+            return matchDAO.addMatch(match);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-
+ 
     @Override
-    public void updateMatch(Match match) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateMatch'");
+    public void updateMatch(Match match) throws SQLException {
+        try {
+            matchDAO.updateMatch(match);
+        } catch (SQLException e) {
+            throw e;
+        }
     }
-
+ 
     @Override
-    public void deleteMatch(int matchId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteMatch'");
+    public void deleteMatch(int matchId) throws SQLException {
+        matchDAO.deleteMatch(matchId);
     }
-
 }
